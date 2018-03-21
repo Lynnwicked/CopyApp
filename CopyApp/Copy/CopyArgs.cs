e73d554b2df949111ee6c46ae5903f2c59a98982
Lynnwicked <lynnwicked420@gmail.com>
2018-03-21 00:11:52 -0600

    general code cleanup

--- CopyApp/Copy/CopyArgs.cs
+++ CopyApp/Copy/CopyArgs.cs
@@ -4,10 +4,10 @@ namespace CopyApp.Copy {
   public class CopyArgs {
     [ArgShortcut("source"), ArgShortcut(ArgShortcutPolicy.ShortcutsOnly), ArgRequired, ArgDescription("Path of source folder"), ArgPosition(1)]
     public string Source { get; set; }
-    
+
     [ArgShortcut("target"), ArgShortcut(ArgShortcutPolicy.ShortcutsOnly), ArgRequired, ArgDescription("Path of target folder"), ArgPosition(2)]
     public string Target { get; set; }
-    
+
     [ArgShortcut("log"), ArgShortcut(ArgShortcutPolicy.ShortcutsOnly), ArgRequired, ArgDescription("Path of log file"), ArgPosition(3)]
     public string Log { get; set; }
 
